f1f1e08ab388c26fefd147495596ae9afb924099
RahafMohammed1 <111027035+RahafMohammed1@users.noreply.github.com>
2023-08-08 21:45:32 +0300

    Add files via upload

---
+++ src/main/java/com/example/tasktrackrsystem/TaskTrackrSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackrSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackrSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tasktrackrsystem/TaskTrackrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
